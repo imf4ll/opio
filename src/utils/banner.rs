acf3a9a1e4da43405dc9841ceb0e4809ea691bcc
z3oxs <z3ox1s@protonmail.com>
2023-07-22 09:07:51 -0300

    feat: First commit

--- src/utils/banner.rs
+++ src/utils/banner.rs
@@ -1,10 +1,10 @@
 pub fn banner() {
-    println!("\x1b[1;31m
+    println!("\x1b[1;34m
                           _________
             _____________ ______  /
             __  ___/  __ `/  __  / 
             _  /   / /_/ // /_/ /  
             /_/    \\__,_/ \\__,_/
     
-       \x1b[1;37mAUR helper & package downgrader\x1b[m\n");
+       \x1b[1;31mAUR helper & package downgrader\x1b[m\n");
 }
