804f5c838cbeef1d682f7ac0efdb035dc6a5bea1
z3oxs <z3ox1s@protonmail.com>
2023-07-22 11:16:33 -0300

    feat: Added about-adapted banner

--- src/utils/banner.rs
+++ src/utils/banner.rs
@@ -8,3 +8,14 @@ pub fn banner() {
     
        \x1b[1;31mAUR helper & package downgrader\x1b[m\n");
 }
+
+pub fn about() -> String {
+    "\x1b[1;31m
+                          _________
+            _____________ ______  /
+            __  ___/  __ `/  __  / 
+            _  /   / /_/ // /_/ /  
+            /_/    \\__,_/ \\__,_/
+    
+       \x1b[1;31mAUR helper & package downgrader\x1b[m".to_string()
+}
