f36776edf7d22266c7c7e7d3f8d16a1c9d35e48a
z3oxs <z3ox1s@protonmail.com>
2023-07-22 11:27:04 -0300

    feat: Added best formating

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ mod utils;
 mod downgrade;
 mod helper;
 
-use std::io::Write;
-
 use crate::utils::{packages::*, log::*, banner::*};
 
 use clap::Parser;
@@ -55,11 +53,11 @@ fn main() {
             let packages = downgrade::get_package(&package_url, &args.package, args.ignore_cache);
 
             if get_current_version(&args.package) == "" {
-                println!("\x1b[1;33m⚠ Package not installed!\x1b[m\n");
+                warning("Package not installed!");
 
             }
             
-            println!("\x1b[1;34m✔ \x1b[1;37m{} \x1b[1;34mversions found.\x1b[m\n", packages.len());
+            success(&format!("\x1b[1;37m{} \x1b[1;34mversions found.", packages.len()));
             
             let package = packages[downgrade::select_package(args.package, &packages)].split(" ").collect::<Vec<&str>>();
 
@@ -77,30 +75,16 @@ fn main() {
             
                 let (package_name, package_version, git_url) = helper::get_package(package_url);
 
-                println!("\x1b[1;34m✔ Package found:\x1b[1;37m {package_name} [{package_version}]\x1b[m\n");
+                success(&format!("Package found:\x1b[1;37m {package_name} [{package_version}]"));
 
                 let exists = get_current_version(&package_name);
 
                 if exists != "" {
-                    println!("\x1b[1;33m⚠ Package already installed:\x1b[1;37m{exists}\x1b[m");
+                    warning(&format!("⚠ Package already installed:\x1b[1;37m{exists}"));
 
                 }
 
-                let mut confirm = String::new();
-
-                print!("\n\x1b[1;33m❓Install package? [y/N]\x1b[m ");
-
-                std::io::stdout().flush().unwrap();
-
-                std::io::stdin().read_line(&mut confirm).unwrap();
-
-                if confirm.trim().to_lowercase() != "y" {
-                    println!("\nAborted by user.");
-
-                    std::process::exit(2);
-                }
-
-                println!("");
+                prompt("Install package? [y/N]");
 
                 helper::install_package(package_name, git_url);
             }
@@ -113,32 +97,18 @@ fn main() {
 
                 }
 
-                println!("\x1b[1;34m✔ \x1b[1;37m{} \x1b[1;34mpackages found with query \x1b[1;37m{}\x1b[m\n", packages.len(), args.package);
+                success(&format!("\x1b[1;37m{} \x1b[1;34mpackages found with query \x1b[1;37m{}", packages.len(), args.package));
 
                 let exists = get_current_version(&args.package);
 
                 if exists != "" {
-                    println!("\x1b[1;33m⚠ Package with same name already installed:\x1b[1;37m{exists}\x1b[m\n");
+                    warning(&format!("Package with same name already installed:\x1b[1;37m{exists}"));
 
                 }
 
                 let package = packages[helper::select_package(&packages)].clone();
 
-                let mut confirm = String::new();
-
-                print!("\n\x1b[1;33m❓Install package? [y/N]\x1b[m ");
-
-                std::io::stdout().flush().unwrap();
-
-                std::io::stdin().read_line(&mut confirm).unwrap();
-
-                if confirm.trim().to_lowercase() != "y" {
-                    println!("\nAborted by user.");
-
-                    std::process::exit(2);
-                }
-
-                println!("");
+                prompt("Install package? [y/N]");
 
                 let package_name = package.split(" ").collect::<Vec<&str>>()[0];
 
