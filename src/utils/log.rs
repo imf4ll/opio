4ef36b4b4e070ad81b058dceab8fc1ff1a928421
z3oxs <z3ox1s@protonmail.com>
2023-07-25 04:41:04 -0300

    fix: Removed unused "prompt" function

--- src/utils/log.rs
+++ src/utils/log.rs
@@ -1,36 +1,21 @@
 use std::process::exit;
-use std::io::{Write, stdin, stdout};
 
 pub static ERROR: &str = "\x1b[1;31m✕\x1b[m";
 
 pub fn error(message: &str) {
-    println!("\x1b[1;31m✕\x1b[m {message}");
+    println!("\x1b[1;31m✘\x1b[m {message}");
 
     exit(2);
 }
 
+pub fn wait(message: &str) {
+    println!("\x1b[1;34m⟳ {message}\x1b[m\n");
+}
+
 pub fn success(message: &str) {
-    println!("\x1b[1;34m✔ {message}\x1b[m\n");
+    println!("\x1b[1;34m› {message}\x1b[m\n");
 }
 
 pub fn warning(message: &str) {
     println!("\x1b[1;33m⚠ {message}\x1b[m\n");
-}
-
-pub fn prompt(message: &str) {
-    let mut confirm = String::new();
-
-    print!("\n\x1b[1;33m❓{message}\x1b[m ");
-
-    stdout().flush().unwrap();
-
-    stdin().read_line(&mut confirm).unwrap();
-
-    if confirm.trim().to_lowercase() != "y" {
-        println!("\nAborted by user.");
-
-        exit(2);
-    }
-
-    println!("");
 }
